5e55af7261997a12172156a7c7b85c3d3ff32944
Adrodoc55 <adrodoc55@googlemail.com>
2015-11-14 17:04:30 +0100

    möglicher test für chaincalculator

---
+++ src/test/java/de/adrodoc55/minecraft/mpl/ChainCalculatorTest.java
@@ -0,0 +1,24 @@
+package de.adrodoc55.minecraft.mpl;
+
+import org.junit.Test;
+
+public class ChainCalculatorTest {
+
+	@Test
+	public void test_calculateChain_Erzeugte_Kette_enthaelt_alle_Commandos() {
+//		// Given:
+//		ChainCalculator factory = new ChainCalculator();
+//		List<Command> commands = new ArrayList<Command>();
+//
+//		// When:
+//		Chain optimal = factory.calculateOptimalChain(commands);
+//		// Then:
+//		List<CommandBlock> blocks = optimal.getCommandBlocks();
+//		List<Command> actual = Lists.transform(blocks, block -> {
+//			return block.getCommand();
+//
+//		});
+//		assertThat(actual).containsExactlyElementsOf(commands);
+	}
+
+}
