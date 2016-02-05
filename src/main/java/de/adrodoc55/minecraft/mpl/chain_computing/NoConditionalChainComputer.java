0b8fe7f730d91ee4e26e25036ae882c8a6d5e2f9
Adrodoc55 <adrodoc55@googlemail.com>
2016-02-05 01:09:16 +0100

    Add Reference to GNU GPL to every source file

--- src/main/java/de/adrodoc55/minecraft/mpl/chain_computing/NoConditionalChainComputer.java
+++ src/main/java/de/adrodoc55/minecraft/mpl/chain_computing/NoConditionalChainComputer.java
@@ -1,3 +1,42 @@
+/*
+ * MPL (Minecraft Programming Language): A language for easy development of commandblock
+ * applications including and IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * This file is part of MPL (Minecraft Programming Language).
+ *
+ * MPL is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MPL. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * MPL (Minecraft Programming Language): Eine Sprache für die einfache Entwicklung von Commandoblock
+ * Anwendungen, beinhaltet eine IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * Diese Datei ist Teil von MPL (Minecraft Programming Language).
+ *
+ * MPL ist Freie Software: Sie können es unter den Bedingungen der GNU General Public License, wie
+ * von der Free Software Foundation, Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * MPL wird in der Hoffnung, dass es nützlich sein wird, aber OHNE JEDE GEWÄHRLEISTUNG,
+ * bereitgestellt; sogar ohne die implizite Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN
+ * BESTIMMTEN ZWECK. Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit MPL erhalten haben. Wenn
+ * nicht, siehe <http://www.gnu.org/licenses/>.
+ */
 package de.adrodoc55.minecraft.mpl.chain_computing;
 
 import java.util.ArrayList;
@@ -33,7 +72,7 @@ public class NoConditionalChainComputer implements ChainComputer {
    * "from pymclevel.nbt import TAG_String\n" + "from pymclevel.nbt import TAG_Byte\n" + "\n" +
    * "displayName = 'Generate " + programFile.getName() + "'\n" + "\n" +
    * "def create_command_block(level, xyz, command, direction='south', mode='chain', conditional=False, auto=True):\n"
-   * + "    x, y, z = xyz\n" + "    		\n" +
+   * + "    x, y, z = xyz\n" + "        \n" +
    * "    idDict = {'impulse' : 137, 'chain' : 211, 'repeat' : 210}\n" +
    * "    blockId = idDict[mode]\n" + "    level.setBlockAt(x, y, z, blockId)\n" + "    \n" +
    * "    damageList = ['down', 'up', 'north', 'south', 'west', 'east']\n" +
--- src/main/java/de/adrodoc55/minecraft/mpl/gui/utils/TabToSpaceTextPM.java
+++ src/main/java/de/adrodoc55/minecraft/mpl/gui/utils/TabToSpaceTextPM.java
@@ -1,3 +1,42 @@
+/*
+ * MPL (Minecraft Programming Language): A language for easy development of commandblock
+ * applications including and IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * This file is part of MPL (Minecraft Programming Language).
+ *
+ * MPL is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MPL. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * MPL (Minecraft Programming Language): Eine Sprache für die einfache Entwicklung von Commandoblock
+ * Anwendungen, beinhaltet eine IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * Diese Datei ist Teil von MPL (Minecraft Programming Language).
+ *
+ * MPL ist Freie Software: Sie können es unter den Bedingungen der GNU General Public License, wie
+ * von der Free Software Foundation, Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * MPL wird in der Hoffnung, dass es nützlich sein wird, aber OHNE JEDE GEWÄHRLEISTUNG,
+ * bereitgestellt; sogar ohne die implizite Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN
+ * BESTIMMTEN ZWECK. Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit MPL erhalten haben. Wenn
+ * nicht, siehe <http://www.gnu.org/licenses/>.
+ */
 package de.adrodoc55.minecraft.mpl.gui.utils;
 
 import java.util.Arrays;
