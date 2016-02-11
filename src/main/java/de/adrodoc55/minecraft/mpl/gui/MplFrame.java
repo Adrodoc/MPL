2e5ac73f3dbcf3c4add2e87b4b33f5e6741f3584
Adrodoc55 <adrodoc55@googlemail.com>
2016-02-11 22:39:28 +0100

    v0.4

--- src/main/java/de/adrodoc55/minecraft/mpl/gui/MplFrame.java
+++ src/main/java/de/adrodoc55/minecraft/mpl/gui/MplFrame.java
@@ -103,7 +103,7 @@ public class MplFrame extends JFrame implements View<MplFramePM>, ModelSubscribe
    * Constructs a new <code>MplFrame</code>.
    */
   public MplFrame() {
-    super("Minecraft Programming Language - v0.3");
+    super("Minecraft Programming Language - v0.4");
     setIconImage(Toolkit.getDefaultToolkit()
         .getImage(MplFrame.class.getResource("/icons/commandblock_icon.png")));
     init();
