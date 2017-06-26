89c287d15fed6a4c04082464a82ed49a99bbf1ce
Adrodoc55 <adrodoc55@googlemail.com>
2017-06-26 18:53:53 +0200

    Add MplSourceViewer

--- ide/src/main/java/de/adrodoc55/minecraft/mpl/ide/fx/editor/MplEditor.java
+++ ide/src/main/java/de/adrodoc55/minecraft/mpl/ide/fx/editor/MplEditor.java
@@ -41,9 +41,6 @@ package de.adrodoc55.minecraft.mpl.ide.fx.editor;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static de.adrodoc55.commons.FileUtils.getFileNameWithoutExtension;
-import static javafx.scene.input.KeyCode.DIGIT7;
-import static javafx.scene.input.KeyCode.S;
-import static javafx.scene.input.KeyCombination.SHORTCUT_DOWN;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -82,12 +79,6 @@ import org.eclipse.fx.text.ui.contentassist.IContextInformationValidator;
 import org.eclipse.fx.text.ui.source.AnnotationPresenter;
 import org.eclipse.fx.text.ui.source.SourceViewer;
 import org.eclipse.fx.text.ui.source.SourceViewerConfiguration;
-import org.eclipse.fx.ui.controls.styledtext.StyledTextArea;
-import org.eclipse.fx.ui.controls.styledtext.StyledTextContent;
-import org.eclipse.fx.ui.controls.styledtext.StyledTextContent.TextChangeListener;
-import org.eclipse.fx.ui.controls.styledtext.TextChangedEvent;
-import org.eclipse.fx.ui.controls.styledtext.TextChangingEvent;
-import org.eclipse.fx.ui.controls.styledtext.TextSelection;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IDocumentPartitioner;
 import org.eclipse.jface.text.source.AnnotationModel;
@@ -113,12 +104,8 @@ import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
 import javafx.scene.control.Control;
-import javafx.scene.input.KeyCodeCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
@@ -127,7 +114,7 @@ import javafx.stage.Window;
 /**
  * @author Adrodoc55
  */
-public class MplEditor extends TextEditor {
+public class MplEditor extends TextEditor implements MplSourceViewer.Context {
   public static MplEditor create(Path path, BorderPane pane, EventBus eventBus,
       MplEditorContext editorContext) {
     MplEditor editor = new MplEditor(editorContext);
@@ -172,27 +159,9 @@ public class MplEditor extends TextEditor {
     return result;
   }
 
-  private class ModifiedListener implements TextChangeListener {
-    @Override
-    public void textChanged(TextChangedEvent event) {
-      modified.set(true);
-    }
-
-    @Override
-    public void textSet(TextChangedEvent event) {
-      modified.set(true);
-    }
-
-    @Override
-    public void textChanging(TextChangingEvent event) {
-      modified.set(true);
-    }
-  }
-
   private final MplEditorContext editorContext;
   private final BooleanProperty modified = new SimpleBooleanProperty(this, "modified");
   private final Property<Input<?>> activeInput = new SimpleObjectProperty<>(this, "activeInput");
-  private final ModifiedListener modifiedListener = new ModifiedListener();
 
   public MplEditor(MplEditorContext editorContext) {
     setInsertSpacesForTab(true);
@@ -204,6 +173,15 @@ public class MplEditor extends TextEditor {
     return modified;
   }
 
+  public boolean isModified() {
+    return modified.get();
+  }
+
+  @Override
+  public void setModified(boolean modified) {
+    this.modified.set(modified);
+  }
+
   public File getFile() {
     LocalFile localFile = (LocalFile) activeInput.getValue();
     Path path = localFile.getPath();
@@ -213,93 +191,13 @@ public class MplEditor extends TextEditor {
   @Override
   public void save() {
     super.save();
-    modified.set(false);
+    setModified(false);
     editorContext.compile(getFile(), true);
   }
 
   @Override
   protected SourceViewer createSourceViewer() {
-    SourceViewer result = super.createSourceViewer();
-    StyledTextArea textWidget = result.getTextWidget();
-    textWidget.addEventHandler(KeyEvent.KEY_PRESSED, this::onKeyPressed);
-    textWidget.contentProperty().addListener(new ChangeListener<StyledTextContent>() {
-      @Override
-      public void changed(ObservableValue<? extends StyledTextContent> observable,
-          StyledTextContent oldValue, StyledTextContent newValue) {
-        if (oldValue != null)
-          oldValue.removeTextChangeListener(modifiedListener);
-        if (newValue != null)
-          newValue.addTextChangeListener(modifiedListener);
-      }
-    });
-    return result;
-  }
-
-  private void onKeyPressed(KeyEvent event) {
-    if (event.isConsumed()) {
-      return;
-    }
-    if (new KeyCodeCombination(DIGIT7, SHORTCUT_DOWN).match(event)) {
-      performCommenting();
-      event.consume();
-      return;
-    }
-    if (new KeyCodeCombination(S, SHORTCUT_DOWN).match(event)) {
-      save();
-      event.consume();
-      return;
-    }
-  }
-
-  private void performCommenting() {
-    StyledTextArea control = getSourceViewer().getTextWidget();
-    StyledTextContent content = control.getContent();
-    int caret = control.getCaretOffset();
-    TextSelection selection = control.getSelection();
-    int firstLineIndex = control.getLineAtOffset(selection.offset);
-    int lastLineIndex = control.getLineAtOffset(selection.offset + selection.length);
-    int firstLineOffset = control.getOffsetAtLine(firstLineIndex);
-    int lastLineOffset = control.getOffsetAtLine(lastLineIndex);
-    int lastLineLength = content.getLine(lastLineIndex).length();
-    int replaceLength = lastLineOffset + lastLineLength - firstLineOffset;
-
-    String text = content.getTextRange(firstLineOffset, replaceLength);
-
-    String prefix = "//";
-    int prefixLength = prefix.length();
-
-    boolean allLinesAreComments = true;
-    for (int lineIndex = firstLineIndex; lineIndex <= lastLineIndex; lineIndex++) {
-      String line = content.getLine(lineIndex);
-      if (!line.trim().startsWith(prefix)) {
-        allLinesAreComments = false;
-        break;
-      }
-    }
-
-    int added = 0;
-    int addedPerLine = allLinesAreComments ? -prefixLength : prefixLength;
-
-    StringBuilder sb = new StringBuilder(text);
-    for (int lineIndex = firstLineIndex; lineIndex <= lastLineIndex; lineIndex++) {
-      int lineOffset = control.getOffsetAtLine(lineIndex) + added;
-      int sbLineOffset = lineOffset - firstLineOffset;
-      if (allLinesAreComments) {
-        String line = content.getLine(lineIndex);
-        int prefixIndex = line.indexOf(prefix);
-        assert prefixIndex >= 0;
-        int sbPrefixIndex = sbLineOffset + prefixIndex;
-        sb.replace(sbPrefixIndex, sbPrefixIndex + prefixLength, "");
-      } else {
-        sb.replace(sbLineOffset, sbLineOffset, prefix);
-      }
-      added += addedPerLine;
-    }
-
-    content.replaceTextRange(firstLineOffset, replaceLength, sb.toString());
-    control.setCaretOffset(selection.offset == caret ? caret + addedPerLine : caret + added);
-    control.setSelectionRange(selection.offset + addedPerLine,
-        selection.length + added - addedPerLine);
+    return new MplSourceViewer(this);
   }
 
   private @Nullable Window getWindow() {
---
+++ ide/src/main/java/de/adrodoc55/minecraft/mpl/ide/fx/editor/MplSourceViewer.java
@@ -0,0 +1,179 @@
+/*
+ * Minecraft Programming Language (MPL): A language for easy development of command block
+ * applications including an IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * This file is part of MPL.
+ *
+ * MPL is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MPL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with MPL. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ *
+ *
+ * Minecraft Programming Language (MPL): Eine Sprache für die einfache Entwicklung von Commandoblock
+ * Anwendungen, inklusive einer IDE.
+ *
+ * © Copyright (C) 2016 Adrodoc55
+ *
+ * Diese Datei ist Teil von MPL.
+ *
+ * MPL ist freie Software: Sie können diese unter den Bedingungen der GNU General Public License,
+ * wie von der Free Software Foundation, Version 3 der Lizenz oder (nach Ihrer Wahl) jeder späteren
+ * veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * MPL wird in der Hoffnung, dass es nützlich sein wird, aber OHNE JEDE GEWÄHRLEISTUNG,
+ * bereitgestellt; sogar ohne die implizite Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN
+ * BESTIMMTEN ZWECK. Siehe die GNU General Public License für weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit MPL erhalten haben. Wenn
+ * nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+package de.adrodoc55.minecraft.mpl.ide.fx.editor;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static javafx.scene.input.KeyCode.DIGIT7;
+import static javafx.scene.input.KeyCode.S;
+import static javafx.scene.input.KeyCombination.SHORTCUT_DOWN;
+
+import org.eclipse.fx.text.ui.source.SourceViewer;
+import org.eclipse.fx.ui.controls.styledtext.StyledTextArea;
+import org.eclipse.fx.ui.controls.styledtext.StyledTextContent;
+import org.eclipse.fx.ui.controls.styledtext.StyledTextContent.TextChangeListener;
+import org.eclipse.fx.ui.controls.styledtext.TextChangedEvent;
+import org.eclipse.fx.ui.controls.styledtext.TextChangingEvent;
+import org.eclipse.fx.ui.controls.styledtext.TextSelection;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * @author Adrodoc55
+ */
+public class MplSourceViewer extends SourceViewer {
+  public interface Context {
+    void setModified(boolean b);
+
+    void save();
+  }
+
+  private final Context context;
+
+  public MplSourceViewer(Context context) {
+    this.context = checkNotNull(context, "context == null!");
+  }
+
+  @Override
+  protected StyledTextArea createTextWidget() {
+    StyledTextArea result = super.createTextWidget();
+    result.addEventHandler(KeyEvent.KEY_PRESSED, this::onKeyPressed);
+    TextChangeListener modifiedListener = new TextChangeListener() {
+      @Override
+      public void textChanged(TextChangedEvent event) {
+        context.setModified(true);
+      }
+
+      @Override
+      public void textSet(TextChangedEvent event) {
+        context.setModified(true);
+      }
+
+      @Override
+      public void textChanging(TextChangingEvent event) {
+        context.setModified(true);
+      }
+    };
+    result.contentProperty().addListener(new ChangeListener<StyledTextContent>() {
+      @Override
+      public void changed(ObservableValue<? extends StyledTextContent> observable,
+          StyledTextContent oldValue, StyledTextContent newValue) {
+        if (oldValue != null)
+          oldValue.removeTextChangeListener(modifiedListener);
+        if (newValue != null)
+          newValue.addTextChangeListener(modifiedListener);
+      }
+    });
+    return result;
+  }
+
+  private void onKeyPressed(KeyEvent event) {
+    if (event.isConsumed()) {
+      return;
+    }
+    if (new KeyCodeCombination(DIGIT7, SHORTCUT_DOWN).match(event)) {
+      performCommenting();
+      event.consume();
+      return;
+    }
+    if (new KeyCodeCombination(S, SHORTCUT_DOWN).match(event)) {
+      save();
+      event.consume();
+      return;
+    }
+  }
+
+  private void performCommenting() {
+    StyledTextArea control = getTextWidget();
+    StyledTextContent content = control.getContent();
+    int caret = control.getCaretOffset();
+    TextSelection selection = control.getSelection();
+    int firstLineIndex = control.getLineAtOffset(selection.offset);
+    int lastLineIndex = control.getLineAtOffset(selection.offset + selection.length);
+    int firstLineOffset = control.getOffsetAtLine(firstLineIndex);
+    int lastLineOffset = control.getOffsetAtLine(lastLineIndex);
+    int lastLineLength = content.getLine(lastLineIndex).length();
+    int replaceLength = lastLineOffset + lastLineLength - firstLineOffset;
+
+    String text = content.getTextRange(firstLineOffset, replaceLength);
+
+    String prefix = "//";
+    int prefixLength = prefix.length();
+
+    boolean allLinesAreComments = true;
+    for (int lineIndex = firstLineIndex; lineIndex <= lastLineIndex; lineIndex++) {
+      String line = content.getLine(lineIndex);
+      if (!line.trim().startsWith(prefix)) {
+        allLinesAreComments = false;
+        break;
+      }
+    }
+
+    int added = 0;
+    int addedPerLine = allLinesAreComments ? -prefixLength : prefixLength;
+
+    StringBuilder sb = new StringBuilder(text);
+    for (int lineIndex = firstLineIndex; lineIndex <= lastLineIndex; lineIndex++) {
+      int lineOffset = control.getOffsetAtLine(lineIndex) + added;
+      int sbLineOffset = lineOffset - firstLineOffset;
+      if (allLinesAreComments) {
+        String line = content.getLine(lineIndex);
+        int prefixIndex = line.indexOf(prefix);
+        assert prefixIndex >= 0;
+        int sbPrefixIndex = sbLineOffset + prefixIndex;
+        sb.replace(sbPrefixIndex, sbPrefixIndex + prefixLength, "");
+      } else {
+        sb.replace(sbLineOffset, sbLineOffset, prefix);
+      }
+      added += addedPerLine;
+    }
+
+    content.replaceTextRange(firstLineOffset, replaceLength, sb.toString());
+    control.setCaretOffset(selection.offset == caret ? caret + addedPerLine : caret + added);
+    control.setSelectionRange(selection.offset + addedPerLine,
+        selection.length + added - addedPerLine);
+  }
+
+  private void save() {
+    context.save();
+  }
+}
